ba7823d9cae2f6ea60f753ca584482ac5f2878a7
jetmirtoshi <jetmir.toshi@fim.edu.al>
2024-05-25 15:00:37 +0200

    Ndryshimet J.Toshi

--- src/main/java/com/sda/car_rental/Model/Entity/Booking.java
+++ src/main/java/com/sda/car_rental/Model/Entity/Booking.java
@@ -1,7 +1,109 @@
 package com.sda.car_rental.Model.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "BOOKING")
 public class Booking {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    // bej ndryshime o Reni
+    @Column(nullable = false)
+    private LocalDateTime bookingDate;
+
+    @Column(nullable = false)
+    private LocalDate dateFrom;
+
+    @Column(nullable = false)
+    private LocalDate dateTo;
+
+    @Column(nullable = false)
+    private double amount;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "customer_id", referencedColumnName = "id")
+    private Customer customer;
+
+    @OneToOne(optional = false)
+    @JoinColumn(name = "car_id", referencedColumnName = "id")
+    private CarList car;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "branch_id", referencedColumnName = "id")
+    private Branches branch;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getBookingDate() {
+        return bookingDate;
+    }
+
+    public void setBookingDate(LocalDateTime bookingDate) {
+        this.bookingDate = bookingDate;
+    }
+
+    public LocalDate getDateFrom() {
+        return dateFrom;
+    }
+
+    public void setDateFrom(LocalDate dateFrom) {
+        this.dateFrom = dateFrom;
+    }
+
+    public LocalDate getDateTo() {
+        return dateTo;
+    }
+
+    public void setDateTo(LocalDate dateTo) {
+        this.dateTo = dateTo;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public CarList getCar() {
+        return car;
+    }
+
+    public void setCar(CarList car) {
+        this.car = car;
+    }
+
+    public Branches getBranch() {
+        return branch;
+    }
+
+    public void setBranch(Branches branch) {
+        this.branch = branch;
+    }
 }
--- src/main/java/com/sda/car_rental/Model/Entity/CarCategory.java
+++ src/main/java/com/sda/car_rental/Model/Entity/CarCategory.java
@@ -1,22 +1,4 @@
 package com.sda.car_rental.Model.Entity;
 
-
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
-@Table(name = "CarCategory")
 public class CarCategory {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String category;
-
-
 }
