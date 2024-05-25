ccd75d70e5c15ea4d06534a90d1730f39f693a59
ReniShehu <reni.shehu@gmail.com>
2024-05-25 15:22:13 +0200

    Ndryshim 2 Reni

--- src/main/java/com/sda/car_rental/Model/Entity/Branches.java
+++ src/main/java/com/sda/car_rental/Model/Entity/Branches.java
@@ -1,10 +1,32 @@
 package com.sda.car_rental.Model.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.boot.autoconfigure.web.WebProperties;
 
+import java.util.List;
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Branches")
 public class Branches {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private long id;
+    @Column(name = "EMPLOYERS")
+    private String employers;
+    @Column(name = "ADRESS")
+    private String adress;
+    @Column(name = "CAR_LIST")
+    private String carList;
+    @Id
+    @Column(name = "COMPANY_ID")
+    private long companyId;
 
 }
