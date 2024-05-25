82202aa1be45e873e23d66f8490fa0601dd91e05
jetmirtoshi <jetmir.toshi@fim.edu.al>
2024-05-25 15:02:48 +0200

    J, toshi 2

--- src/main/java/com/sda/car_rental/Model/Entity/CarCategory.java
+++ src/main/java/com/sda/car_rental/Model/Entity/CarCategory.java
@@ -1,4 +1,21 @@
 package com.sda.car_rental.Model.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "CAR_CATEGORY")
 public class CarCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String category;
 }
