e4e07a98970dea57def9d9a9fc5d130602a87f0b
ReniShehu <reni.shehu@gmail.com>
2024-05-25 15:04:48 +0200

    Ndryshime Reni1

--- src/main/java/com/sda/car_rental/Model/Entity/RentalCompany.java
+++ src/main/java/com/sda/car_rental/Model/Entity/RentalCompany.java
@@ -1,4 +1,76 @@
 package com.sda.car_rental.Model.Entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name ="RENTAL_COMPANY")
+
 public class RentalCompany {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String domain;
+    private String addres;
+    private String owner;
+    private String branches;
+
+    public RentalCompany(long id, String name, String domain, String addres, String owner, String branches) {
+        this.id = id;
+        this.name = name;
+        this.domain = domain;
+        this.addres = addres;
+        this.owner = owner;
+        this.branches = branches;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    public String getAddres() {
+        return addres;
+    }
+
+    public void setAddres(String addres) {
+        this.addres = addres;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getBranches() {
+        return branches;
+    }
+
+    public void setBranches(String branches) {
+        this.branches = branches;
+    }
 }
